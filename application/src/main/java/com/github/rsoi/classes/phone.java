45a32d4915bc6b6d6ec25032cf1c3488846a1dc6
Mincar <pa.vsadnik@gmail.com>
2023-02-16 11:21:47 +0300

    add class phone and interface

---
+++ application/src/main/java/com/github/rsoi/classes/phone.java
@@ -0,0 +1,18 @@
+package com.github.rsoi.classes;
+interface work{
+
+    void print();
+    void compare();
+}
+
+public class phone implements work {
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void compare() {
+
+    }
+}
